2f60af89b008db0de1f4ce03837e00a96c492ac1
Raj Pillai <rajamyown@gmail.com>
2025-09-19 11:49:28 +1000

    corected SA name

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name   = "tf-rg"
-    storage_account_name  = "tf-sa"
+    storage_account_name  = "satfstate2907"
     container_name        = "tf-blob"
     key                   = "main.terraform.tfstate"
   }
