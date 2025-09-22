bb0307660b1a3eebd1af6ae536a7c3677e75589c
Raj Pillai <rajamyown@gmail.com>
2025-09-22 13:43:54 +1000

    deleted firewall

--- main.tf
+++ main.tf
@@ -65,31 +65,3 @@ resource "azurerm_windows_virtual_machine" "example" {
 
 
 
-resource "azurerm_subnet" "firewallsubnet" {
-  name                 = "AzureFirewallSubnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_public_ip" "firewallip" {
-  name                = "firewall-ip"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_firewall" "example" {
-  name                = "testfirewall"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  sku_name            = "AZFW_VNet"
-  sku_tier            = "Standard"
-
-  ip_configuration {
-    name                 = "configuration"
-    subnet_id            = azurerm_subnet.firewallsubnet.id
-    public_ip_address_id = azurerm_public_ip.firewallip.id
-  }
-}
