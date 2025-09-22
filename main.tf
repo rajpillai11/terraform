7f0237ded9f479a60a070c109339989b434c3531
Raj Pillai <rajamyown@gmail.com>
2025-09-22 10:31:40 +1000

    moved backend from backend.tf to main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
-
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "tf-rg"
+    storage_account_name  = "satfstate2907"
+    container_name        = "tf-blob"
+    key                   = "main.terraform.tfstate"
+  }
+}
 
 provider "azurerm" {
     features {}
