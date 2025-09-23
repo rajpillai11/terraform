f53396c03678a98dcaaf5f7c5e7883faa55521cb
Raj Pillai <rajamyown@gmail.com>
2025-09-23 13:06:02 +1000

    output VM's IP address

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-output "Virtual Machine IP address" {
+output "Virtual_Machine_IP address" {
   description = "The public IP address of PROD virtual machine."
   value       = azurerm_windows_virtual_machine.example.public_ip_address
 }
