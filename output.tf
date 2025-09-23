715ae20ad64697a17841969726420e42bb2f96ff
Raj Pillai <rajamyown@gmail.com>
2025-09-23 13:36:26 +1000

    Sensitive output value

--- output.tf
+++ output.tf
@@ -1,4 +1,5 @@
 output "virtual_machine_ip_address" {
   description = "The public IP address of PROD virtual machine."
   value       = azurerm_windows_virtual_machine.example
+  sensitive = true
 }
