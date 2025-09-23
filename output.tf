aa3adbf8deb252be0eb2f5c18114188118f415b6
Raj Pillai <rajamyown@gmail.com>
2025-09-23 13:14:26 +1000

    output VM's attributes

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "virtual_machine_ip_address" {
   description = "The public IP address of PROD virtual machine."
-  value       = azurerm_windows_virtual_machine.example.public_ip_address
+  value       = azurerm_windows_virtual_machine.example
 }
